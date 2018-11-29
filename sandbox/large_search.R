b27a9f07e94094e6af43a64f90ee55b2ed412689
Blake Hansen <blake_hansen@brown.edu>
2018-11-29 11:12:06 -0500

    Finish large_search.R

--- sandbox/large_search.R
+++ sandbox/large_search.R
@@ -1,11 +1,14 @@
 library(rentrez)
 
-max <- min(360, entrez_search(db = "pubmed",term = "Biostatistics[MeSH Major Topic")$count)
+search <-entrez_search(db = "pubmed",term = "(Biostatistics AND Public Health) AND 2015[PDAT]", use_history = T)
 
-
-for( seq_start in seq(1,max,n)){
-  recs <- entrez_fetch(db="nuccore", web_history=snail_coi$web_history,
-                       rettype="fasta", retmax=50, retstart=seq_start)
-  cat(recs, file="snail_coi.fasta", append=TRUE)
-  cat(seq_start+49, "sequences downloaded\r")
+n = 360
+results <- c()
+for(seq_start in seq(1,max,n)){
+  recs <- entrez_summary(db = "pubmed",
+                         term = "(Biostatistics AND Public Health) AND 2015[PDAT]", 
+                         retmax=n, 
+                         retstart=seq_start, 
+                         web_history = search$web_history)
+  results <- c(results, recs)
 }
