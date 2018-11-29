446beedaac0e690dfd368436a991b95f3eb99b0d
Blake Hansen <blake_hansen@brown.edu>
2018-11-29 11:39:50 -0500

    Parse search results into data frame

--- sandbox/large_search.R
+++ sandbox/large_search.R
@@ -1,14 +1,25 @@
 library(rentrez)
+library(stringr)
 
-search <-entrez_search(db = "pubmed",term = "(Biostatistics AND Public Health) AND 2015[PDAT]", use_history = T)
+query <- "(Biostatistics AND Public Health AND HIV) AND 2015[PDAT]"
+search <-entrez_search(db = "pubmed",term = query, use_history = T)
+max <- search$count
 
 n = 360
 results <- c()
 for(seq_start in seq(1,max,n)){
   recs <- entrez_summary(db = "pubmed",
-                         term = "(Biostatistics AND Public Health) AND 2015[PDAT]", 
+                         term = query, 
                          retmax=n, 
                          retstart=seq_start, 
                          web_history = search$web_history)
   results <- c(results, recs)
 }
+
+filter_esummary <- function(data){
+  c(data$title,data$elocationid, data$pubdate, paste(data$authors$name, collapse = ", "), data$pmcrefcount)
+}
+
+filtered_data <- data.frame(matrix(unlist(lapply(results, filter_esummary)), byrow = T, ncol = 5))
+names(filtered_data) <- c("Title", "doi", "date", "authors", "pmc_ref_count")
+filtered_data$date <- as.numeric(str_remove(filtered_data$date, "[A-z]+.+"))
