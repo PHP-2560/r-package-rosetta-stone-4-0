fbf9bae9471363e2c91ed098268b0eb24d25dc7f
Blake Hansen <blake_hansen@brown.edu>
2018-11-29 11:43:55 -0500

    stringsasfactors = Fgit add sandbox/large_search.R git add sandbox/large_search.R

--- sandbox/large_search.R
+++ sandbox/large_search.R
@@ -20,6 +20,6 @@ filter_esummary <- function(data){
   c(data$title,data$elocationid, data$pubdate, paste(data$authors$name, collapse = ", "), data$pmcrefcount)
 }
 
-filtered_data <- data.frame(matrix(unlist(lapply(results, filter_esummary)), byrow = T, ncol = 5))
+filtered_data <- data.frame(matrix(unlist(lapply(results, filter_esummary)), byrow = T, ncol = 5), stringsAsFactors = F)
 names(filtered_data) <- c("Title", "doi", "date", "authors", "pmc_ref_count")
 filtered_data$date <- as.numeric(str_remove(filtered_data$date, "[A-z]+.+"))
