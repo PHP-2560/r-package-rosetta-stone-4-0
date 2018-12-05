930e3836cd54f0b2fe3e521b6e72643891a620d3
Kira Raskina <kira_raskina@brown.edu>
2018-12-05 15:10:48 -0500

    Trends function

--- sandbox/Keyword_trends_line_plot.R
+++ sandbox/Keyword_trends_line_plot.R
@@ -2,30 +2,34 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 
-all.keywords <- read.csv("Total_Keywords.csv", stringsAsFactors = F)
+Key_trends <- function(all.keywords) {
 
-first_year <- names(all.keywords)[4]
-last_year <- names(all.keywords)[ncol(all.keywords)]
-
-tidy_all.keywords <- all.keywords %>% 
-  # reshape the dataframe; each year is not an individual variable now
-  gather(Year, times_used, first_year:last_year) %>%                   
-  # select only the columns we need to build a graph
-  select(Keyword, Year, times_used) %>%   
-  # change NAs to zeros
-  mutate_if(is.numeric, funs(replace(., is.na(.), 0)))  
+  first_year <- names(all.keywords)[4]
+  last_year <- names(all.keywords)[ncol(all.keywords)]
   
-ggplot(tidy_all.keywords, aes(x = Year, y = times_used, group=1, col=Keyword)) + 
-  # make facets by keyword  
-  facet_wrap(~Keyword) +
-  geom_line(size = 1.5, show.legend = FALSE) +
-  # make y axis labels only integers
-  scale_y_discrete(limits=c(0:max(tidy_all.keywords$times_used))) + 
-  # prevent x axis labels overlapping
-  theme(axis.text.x =
-          element_text(size  = 10,
-                       angle = 45,
-                       hjust = 1,
-                       vjust = 1))
+  tidy_all.keywords <- all.keywords %>% 
+    # reshape the dataframe; each year is not an individual variable now
+    gather(Year, times_used, first_year:last_year) %>%                   
+    # select only the columns we need to build a graph
+    select(Keyword, Year, times_used) %>%   
+    # change NAs to zeros
+    mutate_if(is.numeric, funs(replace(., is.na(.), 0)))  
+    
+  ggplot(tidy_all.keywords, aes(x = gsub("[^0-9\\.]", "", Year), y = times_used, group=1, col=Keyword)) + 
+    xlab("Year") +
+    ylab("Times used") +
+    # make facets by keyword  
+    facet_wrap(~Keyword) +
+    geom_line(size = 1.5, show.legend = FALSE) +
+    # make y axis labels only integers
+    scale_y_discrete(limits=c(0:max(tidy_all.keywords$times_used))) + 
+    # prevent x axis labels overlapping
+    theme(axis.text.x =
+            element_text(size  = 9,
+                         angle = 90,
+                         hjust = 0.5,
+                         vjust = 0.5))
+}
 
+Key_trends(read.csv("Data/Total_Keywords.csv", stringsAsFactors = F))
 
