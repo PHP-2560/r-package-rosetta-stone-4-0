50c6d04342c36d5f200251461736fa5d53096c3f
Devone <fdevone@ad.brown.edu>
2018-12-05 15:21:13 -0500

    Keyword search table functioning

--- sandbox/grab_keywords_simplified_FromDataFrame_DeVone.R
+++ sandbox/grab_keywords_simplified_FromDataFrame_DeVone.R
@@ -19,89 +19,34 @@ group_by(date)
 years<-unique(Keylist$date)
 years <- tolower(years[!is.na(years)])
 
-for(i in 1:length(years)){
-print(years[i])
-  
 Filtered_Keylist<-Keylist %>%
-filter(date==years[i]) %>%
-select(keywords)
-  
-assign(paste("Keywords_",years[i], sep = ""),Filtered_Keylist)
-  
-}
+  filter(date==years[1])
 
-Unlisted<-unlist(paste("Keywords_",years[1], sep = ""))
+Unlisted<- unlist(Filtered_Keylist$keywords)
+
 keyword_count_df <- as.data.frame(table(Unlisted[Unlisted!=""]), 
                                   stringsAsFactors = FALSE)  
 Full_table<-keyword_count_df[order(keyword_count_df$Freq, decreasing = TRUE),]
 
 for(i in 2:length(years)){
-
-#assign(paste("Unlisted_Keywords_",years[i], sep = ""),unlist(paste("Keywords_",years[i], sep = "")))
-assign(paste(Unlisted,years[i], sep = ""),unlist(paste("Keywords_",years[i], sep = "")))
-
-keyword_count_df <- as.data.frame(table(Unlisted[Unlisted!=""]), 
-                                    stringsAsFactors = FALSE)  
-
-keyword_table_year <- keyword_count_df[order(keyword_count_df$Freq, decreasing = TRUE),]
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# count frequency of each word, excluding empty strings
-
-
-# sort the table by decreasing Freq
-keyword_count_df <- keyword_count_df[order(keyword_count_df$Freq, decreasing = TRUE),]
-
-
-#Simplified way to grab keywords of all articles from a given pubmed
-
-#NOTE: I had a 2014 search with no hits, I got a error message but we need to make the break or make an empty file casue it just used the save 2015 data
-
-#Note from Frank: Included search variable 
-term <- "Biostatistics[MeSH Term] AND sugar"
-year<-2016
-search <-paste(term, "AND (", year, "[PDAT])")
-
-n=10000 #Set number of search results wanted
-vivax_search <- entrez_search(db = "pubmed",
-                              term = search, retmax=n) #Altered to view biostats
-
-multi_summs <- entrez_summary(db="pubmed", id=vivax_search$ids, retmax=n)
-
-date_and_cite <- extract_from_esummary(multi_summs,"uid") ##CAN GET CITATIONS
-
-get_keywords <- function(x){
-  url<-paste("https://www.ncbi.nlm.nih.gov/pubmed/",x, sep="" )
   
-  #This body of code grabs the keylist
-  results <- read_html(url)
-  papers <- html_nodes(results, ".rprt")
-  keys_html <- html_nodes(papers, ".keywords")
-  keys <- html_text(keys_html) %>%
-          str_remove("KEYWORDS: ") %>%
-          str_split("; ")#Note from Frank: Fixed to include space
+  Filtered_Keylist<-Keylist %>%
+    filter(date==years[i])
+  
+  Unlisted<- unlist(Filtered_Keylist$keywords)
+  
+  keyword_count_df <- as.data.frame(table(Unlisted[Unlisted!=""]), 
+                                    stringsAsFactors = FALSE)  
+  To_Join<-keyword_count_df[order(keyword_count_df$Freq, decreasing = TRUE),]
+  
+  Full_table <- full_join(Full_table,To_Join, by = "Var1")
 }
 
-keywords <- as.data.frame(table(unlist(lapply(date_and_cite, get_keywords))))
-write.csv(keywords, file = paste("Data/Keywords_",search,".csv",sep=""))#Note from Frank: Included file output for table merge
+
+name_list<-c("Keyword")
+for(i in 1:length(years)){
+  
+name_list[length(name_list)+1]<-years[i]}
+
+names(Full_table)<-name_list
+
