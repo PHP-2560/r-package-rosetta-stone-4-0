1d5baffe74a0188885b96ffb9b2e3a13ffe525a2
nadia-mercado <nadia_mercado@brown.edu>
2018-12-10 12:01:40 -0500

    changed tabs

--- sandbox/pubkey/app.R
+++ sandbox/pubkey/app.R
@@ -1,14 +1,36 @@
-
 library(shiny)
+library(shinydashboard)
 
-  ui <- fluidPage(titlePanel("pubkey"),
-    navlistPanel(
-      tabPanel("Introduction to pubkey", "include instructional video here"),
-      tabPanel("Search PubMed", "contents"),
-      tabPanel("Data Table", "contents"),
-      tabPanel("Keyword Bar Graph"), 
-      tabPanel("Keyword Trends Line Graph")
-    ))
+  ui <- dashboardPage(
+      header <- dashboardHeader(title = "PubKey"),
+      sidebar <- dashboardSidebar(
+        sidebarMenu(
+          menuItem("Dashboard", tabname = "dashboard", icon = icon("dashboard")), 
+          menuItem("Instructions", icon = icon("list", lib='glyphicon'), href = "https://www.ncbi.nlm.nih.gov/pubmed/"),
+          menuItem("Search PubMed", icon = icon("flash"), lib='glyphicon'), 
+          menuItem("Data Table", icon = icon("th"), lib='glyphicon'),
+          menuItem("Keyword Bar Graph", icon = icon("signal"), lib = 'glyphicon'),
+          menuItem("Keyword Trends Line Graph", icon = icon("glass"), lib = 'glyphicon')
+        )
+      ),
+      
+      
+      
+      dashboardBody()
+    
+    
+    
+  )
+    
+    
+    # fluidPage(titlePanel("pubkey"),
+    # navlistPanel(
+    #   tabPanel("Introduction to pubkey", "include instructional video here"),
+    #   tabPanel("Search PubMed", "contents"),
+    #   tabPanel("Data Table", "contents"),
+    #   tabPanel("Keyword Bar Graph"), 
+    #   tabPanel("Keyword Trends Line Graph")
+    # ))
   
 
   server <- function(input, output){
