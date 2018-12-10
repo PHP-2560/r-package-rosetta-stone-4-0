cb804b2efe6b0816ffd63436a27606f030666ee5
nadia-mercado <nadia_mercado@brown.edu>
2018-12-10 16:18:45 -0500

    new attempt to add content in tabs

--- sandbox/pubkey/app.R
+++ sandbox/pubkey/app.R
@@ -1,50 +1,36 @@
 library(shiny)
 library(shinydashboard)
 
-  ui <- dashboardPage(
-      header <- dashboardHeader(title = "PubKey"),
-      sidebar <- dashboardSidebar(
-        sidebarMenu(
-          #menuItem("Dashboard", tabname = "dashboard", icon = icon("dashboard")), 
-          menuItem("Instructions", tabName = "Instructions", icon = icon("list"), lib='glyphicon'),
-          menuItem("Search PubMed", tabName = "Search PubMed", icon = icon("search"), lib='glyphicon'), 
-          menuItem("Data Table", tabName =  "Data Table", icon = icon("calendar"), lib='glyphicon'),
-          menuItem("Keyword Bar Graph", tabName = "Keyword Bar Graph", icon = icon("signal"), lib = 'glyphicon'),
-          menuItem("Keyword Trends Line Graph", tabName = "Keyword Trends Line Graph", icon = icon("globe"), lib = 'glyphicon'),
-          menuItem("Go to PubMed!", icon = icon("send"), lib='glyphicon'), href = "https://www.ncbi.nlm.nih.gov/pubmed/") 
+ui <- 
+dashboardPage(
+      dashboardHeader(title = "PubKey"),
+       
+      #dashboard sidebar names
+sidebar <- dashboardSidebar(
+    sidebarMenu(id = "sbmenu",
+          menuItem("Instructions", tabName = "instructions", icon = icon("list")),
+          menuItem("Search PubMed", tabName = "search_pubmed", icon = icon("search")), 
+          menuItem("Data Table", tabName =  "data table", icon = icon("calendar")),
+          menuItem("Keyword Bar Graph", tabName = "keyword bar graph", icon = icon("signal")),
+          menuItem("Keyword Trends Line Graph", tabName = "keyword trends line graph", icon = icon("globe")),
+          menuItem("Go to PubMed!", icon = icon("send")), href = "https://www.ncbi.nlm.nih.gov/pubmed/") 
       ),
       
-     body <- dashboardBody(
+    #adding content to each of the tabs
+body <- dashboardBody(
         tabItems(
           #First tab content
-          tabItem(tabName = "Instructions", "Instructions tab content"), 
+          tabItem("instructions", "Instructions tab content")), 
           
           #2nd tab content
-          tabItem(tabName = "Search PubMed", 
-                  h2("add content here"))
-                    
-                  
+          tabItem("search_pubmed", "add video here"))
           )
-        
-      )
-    
-    
-    
-  )
-    
-    
-    # fluidPage(titlePanel("pubkey"),
-    # navlistPanel(
-    #   tabPanel("Introduction to pubkey", "include instructional video here"),
-    #   tabPanel("Search PubMed", "contents"),
-    #   tabPanel("Data Table", "contents"),
-    #   tabPanel("Keyword Bar Graph"), 
-    #   tabPanel("Keyword Trends Line Graph")
-    # ))
+
   
 
+
   server <- function(input, output){
-    
+    observe(print(input$sbmenu))
     
   }
 # Run the application 
