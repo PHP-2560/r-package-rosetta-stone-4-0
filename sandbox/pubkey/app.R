ca6e992842475e275207daadb7af2a13c40d699a
nadia-mercado <nadia_mercado@brown.edu>
2018-12-10 14:12:32 -0500

    corrected tab names and beginning code to add input to each tab

--- sandbox/pubkey/app.R
+++ sandbox/pubkey/app.R
@@ -5,18 +5,28 @@ library(shinydashboard)
       header <- dashboardHeader(title = "PubKey"),
       sidebar <- dashboardSidebar(
         sidebarMenu(
-          menuItem("Dashboard", tabname = "dashboard", icon = icon("dashboard")), 
-          menuItem("Instructions", icon = icon("list", lib='glyphicon'), href = "https://www.ncbi.nlm.nih.gov/pubmed/"),
-          menuItem("Search PubMed", icon = icon("flash"), lib='glyphicon'), 
-          menuItem("Data Table", icon = icon("th"), lib='glyphicon'),
-          menuItem("Keyword Bar Graph", icon = icon("signal"), lib = 'glyphicon'),
-          menuItem("Keyword Trends Line Graph", icon = icon("glass"), lib = 'glyphicon')
-        )
+          #menuItem("Dashboard", tabname = "dashboard", icon = icon("dashboard")), 
+          menuItem("Instructions", tabName = "Instructions", icon = icon("list"), lib='glyphicon'),
+          menuItem("Search PubMed", tabName = "Search PubMed", icon = icon("search"), lib='glyphicon'), 
+          menuItem("Data Table", tabName =  "Data Table", icon = icon("calendar"), lib='glyphicon'),
+          menuItem("Keyword Bar Graph", tabName = "Keyword Bar Graph", icon = icon("signal"), lib = 'glyphicon'),
+          menuItem("Keyword Trends Line Graph", tabName = "Keyword Trends Line Graph", icon = icon("globe"), lib = 'glyphicon'),
+          menuItem("Go to PubMed!", icon = icon("send"), lib='glyphicon'), href = "https://www.ncbi.nlm.nih.gov/pubmed/") 
       ),
       
-      
-      
-      dashboardBody()
+     body <- dashboardBody(
+        tabItems(
+          #First tab content
+          tabItem(tabName = "Instructions", "Instructions tab content"), 
+          
+          #2nd tab content
+          tabItem(tabName = "Search PubMed", 
+                  h2("add content here"))
+                    
+                  
+          )
+        
+      )
     
     
     
