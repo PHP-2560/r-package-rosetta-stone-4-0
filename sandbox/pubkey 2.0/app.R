06a332157b0160db4251762e50f4d290cf689a16
Kira Raskina <kira_raskina@brown.edu>
2018-12-10 22:45:37 -0500

    PubMed Advanced Search embeded into Pubmed Search tab

--- sandbox/pubkey 2.0/app.R
+++ sandbox/pubkey 2.0/app.R
@@ -20,7 +20,7 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       tabItem("Instructions", h1("How to use PubKey")),
-      tabItem("Search_PubMed", h1("key_download()")),
+      tabItem("Search_PubMed", h1("key_download()"), htmlOutput("PubSearch")),
       tabItem("data_table", h1("key_summary()")),
       tabItem("keyword_bar_graph", h1("key_bgraph()")),
       tabItem("keyword_trends_line_graph", h1("key_lgraph()"))
@@ -30,7 +30,15 @@ ui <- dashboardPage(
 
 
 server <- function(input, output) {
-  observe(print(input$sbmenu))
+  getPage<-function() {
+    return(tags$iframe(src = "https://www.ncbi.nlm.nih.gov/pubmed/advanced", 
+                       style="width:100%;",  frameborder="0",
+                       id="iframe",
+                       height = "600px"))
+  }
+  output$PubSearch <-renderUI({
+    getPage()
+  })
 }
 
 shinyApp(ui,server)
