14d9a79a701eb53ab244ec6e5fc130f40c34b5c6
nadia-mercado <nadia_mercado@brown.edu>
2018-12-12 16:08:26 -0500

    updated with iframe

--- sandbox/pubkey/app.R
+++ sandbox/pubkey/app.R
@@ -1,38 +1,66 @@
 library(shiny)
 library(shinydashboard)
 
-ui <- 
-dashboardPage(
-      dashboardHeader(title = "PubKey"),
-       
-      #dashboard sidebar names
-sidebar <- dashboardSidebar(
-    sidebarMenu(id = "sbmenu",
-          menuItem("Instructions", tabName = "instructions", icon = icon("list")),
-          menuItem("Search PubMed", tabName = "search_pubmed", icon = icon("search")), 
-          menuItem("Data Table", tabName =  "data table", icon = icon("calendar")),
-          menuItem("Keyword Bar Graph", tabName = "keyword bar graph", icon = icon("signal")),
-          menuItem("Keyword Trends Line Graph", tabName = "keyword trends line graph", icon = icon("globe")),
-          menuItem("Go to PubMed!", icon = icon("send")), href = "https://www.ncbi.nlm.nih.gov/pubmed/") 
-      ),
-      
-    #adding content to each of the tabs
-body <- dashboardBody(
-        tabItems(
-          #First tab content
-          tabItem("instructions", "Instructions tab content")), 
-          
-          #2nd tab content
-          tabItem("search_pubmed", "add video here"))
-          )
+library(dplyr)
+library(stringr)
+library(rvest)
+library(rebus)
 
+library(pubkey)
+library(rentrez)
+
+ui <- dashboardPage(
+  dashboardHeader(
+    title = "PubKey"
+  ),
   
+  dashboardSidebar(
+    sidebarMenu(id="sbmenu",
+                menuItem("Instructions", tabName = "Instructions", icon = icon("list")),
+                menuSubItem("Estimate Time", tabName = "Estimate_Time", icon = icon("clock")),
+                menuItem("Search PubMed", tabName = "Search_PubMed", icon = icon("search")),
+                menuItem("Data Table", tabName =  "data_table", icon = icon("calendar")),
+                menuItem("Keyword Bar Graph", tabName = "keyword_bar_graph", icon = icon("signal")),
+                menuItem("Keyword Trends Line Graph", tabName = "keyword_trends_line_graph", icon = icon("globe")),
+                menuItem("Go to PubMed!", icon = icon("send")), href = "https://www.ncbi.nlm.nih.gov/pubmed/")
+  ),
+  
+  dashboardBody(
+    tabItems(
+      tabItem("Instructions", h1("How to use PubKey")),
+      tabItem("Estimate_Time", h1("Estimate the time it will take to complete your query"), 
+              textInput("example_Search_Term", label="Enter search term"), 
+              textOutput("time_estimate")),
+      tabItem("Search_PubMed", h1("Download your initial dataframe"), 
+              textInput("Search_Term", label="Enter search term"),
+              tags$iframe(src = "https://www.ncbi.nlm.nih.gov/pubmed/advanced", 
+                                                                                                                     style="width:100%;",  frameborder="0",
+                                                                                                                     id="iframe",
+                                                                                                                     height = "600px"),
+              dataTableOutput("raw_data")),
+      tabItem("data_table", h1("key_summary()"),dataTableOutput("data")),
+      tabItem("keyword_bar_graph", h1("key_bgraph()")),
+      tabItem("keyword_trends_line_graph", h1("key_lgraph()"))
+    )
+  )
+)
 
 
-  server <- function(input, output){
-    observe(print(input$sbmenu))
-    
+
+server <- function(input, output) {
+  getPage<-function() {
+    return(tags$iframe(src = "https://www.ncbi.nlm.nih.gov/pubmed/advanced", 
+                       style="width:100%;",  frameborder="0",
+                       id="iframe",
+                       height = "600px"))
   }
-# Run the application 
-shinyApp(ui = ui, server = server)
+  output$PubSearch <-renderUI({
+    getPage()
+  })
+  
+  output$time_estimate <- renderText({key_estimate_time(input$example_Search_Term)})
+  
+  output$raw_data <- renderDataTable({key_download(input$Search_Term)})
+}
 
+shinyApp(ui,server)
